e39768c7b417e56749dd52b10f2fa290253ef2a9
ModyQyW <wurui.dev@gmail.com>
2024-04-15 16:33:02 +0800

    feat: add total

--- app.vue
+++ app.vue
@@ -46,6 +46,7 @@ const handleFileInputChange = (event: Event) => {
       <div class="mt-4"></div>
       <p>You have uploaded {{ files.length }} files.</p>
       <ul class="list-disc pl-4">
+        <li>Total: {{ list.reduce((acc, cur) => acc + cur.count, 0) }}h</li>
         <li v-for="item of list" :key="item.name">
           {{ item.name }} - {{ item.count }}h
         </li>
